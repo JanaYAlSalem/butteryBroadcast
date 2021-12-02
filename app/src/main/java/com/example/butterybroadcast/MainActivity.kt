53ed6a45fe7d2a60825bca4631b7df00be6f2526
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2021-12-02 09:31:50 +0300

    design is updated

--- app/src/main/java/com/example/butterybroadcast/MainActivity.kt
+++ app/src/main/java/com/example/butterybroadcast/MainActivity.kt
@@ -4,10 +4,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.PorterDuff
 import android.os.BatteryManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
 import com.example.butterybroadcast.databinding.ActivityMainBinding
 
@@ -28,25 +31,32 @@ class MainActivity : AppCompatActivity() {
 
     private val batteryBroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
 
-        override fun onReceive(context: Context?, intent: Intent?) {
+        @RequiresApi(Build.VERSION_CODES.M)
+        override fun onReceive(context: Context, intent: Intent?) {
 
             if (intent?.action == "android.intent.action.BATTERY_CHANGED") {
                 val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
 
-                if (level > 15)
+                if (level > 20) {
                     LinkXML.charginglevel.setText("BATTERY is $level")
-                else
-                    LinkXML.charginglevel.setText(
-                        "low Battery \n " +
-                                "$level % Battery remaining"
-                    )
-
+                    LinkXML.iconBColor.setColorFilter(context.getColor(R.color.grean))
+                } else {
+                    LinkXML.charginglevel.setText("low Battery \n " +
+                                "$level % Battery remaining")
+                    LinkXML.iconBColor.setColorFilter(context.getColor(R.color.red))
+                }
             }
 
             val status: Int = intent?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
 
             LinkXML.isCharge.isVisible = if (status == BatteryManager.BATTERY_STATUS_CHARGING
                 || status == BatteryManager.BATTERY_STATUS_FULL) { true } else { false }
+
+           if( LinkXML.isCharge.isVisible){
+               LinkXML.iconBColor.isVisible = false
+           } else
+               LinkXML.iconBColor.isVisible = true
+
 
         } // end onReceive fun
     }
